9225938fe47e32cf5b11f86fe23c25675faa6ab0
Cortexi <julietcsampson@gmail.com>
2022-05-14 23:37:25 -0500

    add rows and adjust grid placement

--- src/Pathfinder/Pathfinder.jsx
+++ src/Pathfinder/Pathfinder.jsx
@@ -14,7 +14,7 @@ export default class Pathfinder extends Component {
 
     componentDidMount() {
         const nodes = [];
-        for (let row = 0; row < 15; row++) {
+        for (let row = 0; row < 20; row++) {
             const currRow = [];
             for (let col = 0; col < 50; col++) {
                 const currNode = {
