0fab1ab70f54689654f546fc92c5e5203feebe14
Cortexi <julietcsampson@gmail.com>
2022-05-14 22:49:37 -0500

    created files

---
+++ src/Pathfinder/Pathfinder.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import Node from '.Node/Node';
+
+import './Pathfinder.css';
+import { render } from '@testing-library/react';
+
+export default class Pathfinder extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                Foo
+                <Node></Node>
+            </div>
+        );
+    }
+}
