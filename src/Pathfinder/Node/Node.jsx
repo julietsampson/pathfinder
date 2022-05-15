6de34ffeeee821ed7214e0ef1c077b7a61542ec0
Cortexi <julietcsampson@gmail.com>
2022-05-14 23:36:54 -0500

    fix issues with start and end node not appearing

--- src/Pathfinder/Node/Node.jsx
+++ src/Pathfinder/Node/Node.jsx
@@ -10,7 +10,7 @@ export default class Node extends Component {
 
     render() {
         const {isEndNode, isStartNode} = this.props;
-        const extraClassName = isEndNode ? 'node-end' : isStartNode ? 'node-start' : '';
+        const extraClassName = isEndNode ? '-end' : isStartNode ? '-start' : '';
         return <div className={`node${extraClassName}`}></div>;
     }
 }
