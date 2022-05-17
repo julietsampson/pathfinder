2e9491a0dccbb502af27ec8f53e35269f086c24f
Cortexi <julietcsampson@gmail.com>
2022-05-17 00:05:40 -0500

    add attributes for wall nodes to node.jsx

--- src/Pathfinder/Node/Node.jsx
+++ src/Pathfinder/Node/Node.jsx
@@ -3,25 +3,32 @@ import React, {Component} from 'react';
 import './Node.css'
 
 export default class Node extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
     render() {
-        const {isEndNode, isStartNode, visited} = this.props;
+        const {
+            col,
+            isEndNode, 
+            isStartNode,
+            isWall,
+            onMouseDown,
+            onMouseEnter,
+            onMouseUp,
+            row,
+        } = this.props;
         const extraClassName = isEndNode 
         ? 'node-end' 
         : isStartNode 
         ? 'node-start'
-        : visited
-        ? 'node-visited' 
+        : isWall
+        ? 'node-wall' 
         : '';
-        return <div className={`node ${extraClassName}`}></div>;
+        return (
+            <div
+                id={`node-${row}-${col}`} 
+                className={`node ${extraClassName}`}
+                onMouseDown={() => onMouseDown(row, col)}
+                onMouseEnter={() => onMouseEnter(row, col)}
+                onMouseUp={() => onMouseUp(row, col)}
+            ></div>
+        );
     }
 }
-
-export const DEFAULT_NODE = {
-    row: 0,
-    col: 0,
-};
